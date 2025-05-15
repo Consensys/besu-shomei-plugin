0a27f282c2ef9ccda887b722ace1eb15f16b5ecf
garyschulte <garyschulte@gmail.com>
2025-05-15 12:52:33 -0700

    test for 4337 contract creation revert
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- src/test/java/net/consensys/shomei/blocktracing/ZkBlockImportTracerProviderTest.java
+++ src/test/java/net/consensys/shomei/blocktracing/ZkBlockImportTracerProviderTest.java
@@ -14,6 +14,17 @@
  */
 package net.consensys.shomei.blocktracing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import net.consensys.linea.zktracer.ZkTracer;
 import net.consensys.shomei.cli.ShomeiCliOptions;
 import net.consensys.shomei.context.TestShomeiContext;
@@ -21,17 +32,21 @@ import net.consensys.shomei.trielog.TrieLogValue;
 import net.consensys.shomei.trielog.ZkAccountValue;
 import net.consensys.shomei.trielog.ZkTrieLogFactory;
 import net.consensys.shomei.trielog.ZkTrieLogService;
+
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt256;
 import org.bouncycastle.math.ec.custom.sec.SecP256K1FieldElement;
-import org.hyperledger.besu.crypto.KeyPair;
-import org.hyperledger.besu.crypto.SECPPrivateKey;
-import org.hyperledger.besu.crypto.SECPPublicKey;
 import org.hyperledger.besu.crypto.SECPSignature;
 import org.hyperledger.besu.datatypes.AccountValue;
 import org.hyperledger.besu.datatypes.Address;
-import org.hyperledger.besu.datatypes.GWei;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.StorageSlotKey;
 import org.hyperledger.besu.datatypes.TransactionType;
@@ -44,10 +59,7 @@ import org.hyperledger.besu.ethereum.core.BlockchainSetupUtil;
 import org.hyperledger.besu.ethereum.core.MutableWorldState;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.ethereum.core.TransactionReceipt;
-import org.hyperledger.besu.ethereum.core.TransactionTestFixture;
 import org.hyperledger.besu.ethereum.mainnet.BlockProcessor;
-import org.hyperledger.besu.ethereum.storage.keyvalue.KeyValueSegmentIdentifier;
-import org.hyperledger.besu.ethereum.trie.pathbased.bonsai.worldview.BonsaiWorldState;
 import org.hyperledger.besu.plugin.ServiceManager;
 import org.hyperledger.besu.plugin.data.BlockHeader;
 import org.hyperledger.besu.plugin.services.BlockImportTracerProvider;
@@ -62,24 +74,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.math.BigInteger;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class ZkBlockImportTracerProviderTest {
@@ -248,7 +242,6 @@ public class ZkBlockImportTracerProviderTest {
     verify(comparator, times(1)).alert(any());
   }
 
-
   @Test
   public void assertTrieLogContainsHubStateOnNewContractSload() {
     testContext.getCliOptions().zkTraceComparisonMask = 15;
@@ -260,28 +253,31 @@ public class ZkBlockImportTracerProviderTest {
     doAnswer(__ -> Optional.of(zkTrieLogService))
         .when(mockPluginServiceManager)
         .getService(TrieLogService.class);
-    doAnswer(__ -> Optional.of(zkTrieLogFactoryImpl))
-        .when(zkTrieLogService)
-        .getTrieLogFactory();
+    doAnswer(__ -> Optional.of(zkTrieLogFactoryImpl)).when(zkTrieLogService).getTrieLogFactory();
     AtomicReference<TrieLog> capturedTrieLog = new AtomicReference<>();
-    doAnswer(invocation -> {
-      var trielog = (TrieLog) invocation.callRealMethod();
-      capturedTrieLog.set(trielog);
-      return trielog;
-    }).when(zkTrieLogFactoryImpl).create(any(TrieLogAccumulator.class), any(BlockHeader.class));
+    doAnswer(
+            invocation -> {
+              var trielog = (TrieLog) invocation.callRealMethod();
+              capturedTrieLog.set(trielog);
+              return trielog;
+            })
+        .when(zkTrieLogFactoryImpl)
+        .create(any(TrieLogAccumulator.class), any(BlockHeader.class));
 
-    var zkTracerProviderSpy = spy(new ZkBlockImportTracerProvider(
-        testContext, () -> Optional.of(BigInteger.valueOf(59144L))));
+    var zkTracerProviderSpy =
+        spy(
+            new ZkBlockImportTracerProvider(
+                testContext, () -> Optional.of(BigInteger.valueOf(59144L))));
     testContext.setBlockImportTraceProvider(zkTracerProviderSpy);
 
-
     // Set up test world state and blockchain
-    final BlockchainSetupUtil setupUtil = BlockchainSetupUtil.createForEthashChain(
-        new BlockTestUtil.ChainResources(
-        this.getClass().getClassLoader().getResource("zktestGenesis.json"),
-        BlockTestUtil.class.getClassLoader().getResource("chain.blocks")),
-        DataStorageFormat.BONSAI,
-        mockPluginServiceManager);
+    final BlockchainSetupUtil setupUtil =
+        BlockchainSetupUtil.createForEthashChain(
+            new BlockTestUtil.ChainResources(
+                this.getClass().getClassLoader().getResource("zktestGenesis.json"),
+                BlockTestUtil.class.getClassLoader().getResource("chain.blocks")),
+            DataStorageFormat.BONSAI,
+            mockPluginServiceManager);
     final Blockchain blockchain = setupUtil.getBlockchain();
     final MutableWorldState worldState = setupUtil.getWorldArchive().getWorldState();
 
@@ -292,55 +288,61 @@ public class ZkBlockImportTracerProviderTest {
     doAnswer(__ -> Optional.of(zkTracerProviderSpy))
         .when(mockPluginServiceManager)
         .getService(BlockImportTracerProvider.class);
-    doAnswer(__ -> mockPluginServiceManager)
-        .when(spyProtocolContext)
-        .getPluginServiceManager();
-    doAnswer(invocation -> {
-      BlockAwareOperationTracer tracer = (BlockAwareOperationTracer) invocation.callRealMethod();
-      capturedTracer.set((ZkTracer) tracer);
-      return tracer;
-    }).when(zkTracerProviderSpy).getBlockImportTracer(any(BlockHeader.class));
+    doAnswer(__ -> mockPluginServiceManager).when(spyProtocolContext).getPluginServiceManager();
+    doAnswer(
+            invocation -> {
+              BlockAwareOperationTracer tracer =
+                  (BlockAwareOperationTracer) invocation.callRealMethod();
+              capturedTracer.set((ZkTracer) tracer);
+              return tracer;
+            })
+        .when(zkTracerProviderSpy)
+        .getBlockImportTracer(any(BlockHeader.class));
 
-
-    var r = Bytes.fromHexString("0xde0db8ce81c8092823a2d91b3a3cc421bc6b384b562f567e67b26c5443ff28e9").toUnsignedBigInteger();
-    var s = Bytes.fromHexString("0x44457e69f445284d1f1ba99107b53c1d4b3d1e8c7283ff504341e5a92699c09d").toUnsignedBigInteger();
+    var r =
+        Bytes.fromHexString("0xde0db8ce81c8092823a2d91b3a3cc421bc6b384b562f567e67b26c5443ff28e9")
+            .toUnsignedBigInteger();
+    var s =
+        Bytes.fromHexString("0x44457e69f445284d1f1ba99107b53c1d4b3d1e8c7283ff504341e5a92699c09d")
+            .toUnsignedBigInteger();
     var sig = SECPSignature.create(r, s, (byte) 0, SecP256K1FieldElement.Q);
 
-    var tx = new Transaction.Builder()
-        .signature(sig)
-        .type(TransactionType.EIP1559)
-        .sender(Address.fromHexString("0x43370108f30ee5ed54a9565f37af3be8502903f5"))
-        .to(Address.fromHexString("0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789"))
-        .nonce(14261)
-        .value(Wei.ZERO)
-        .payload(Bytes.fromHexString("0x1fad948c000000000000000000000000000000000000000000000000000000000000004000000000000000000000000043370108f30ee5ed54a9565f37af3be8502903f50000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000051ce6a8e868fa0c928346ac7489a74432c44bc933db076b8929744925f16e35a14de0c7e122e512efe282e76000000000000000000000000000000000000000000000000000000000000000000000000000001600000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000007a120000000000000000000000000000000000000000000000000000000000006b08a000000000000000000000000000000000000000000000000000000000000b1580000000000000000000000000000000000000000000000000000000065fa53ff000000000000000000000000000000000000000000000000000000005cb5068300000000000000000000000000000000000000000000000000000000000002c000000000000000000000000000000000000000000000000000000000000002e000000000000000000000000000000000000000000000000000000000000000783fbe35a874284e41c955331a363c1ea085301a8dd8fd8f440000000000000000000000001708dc05842c8fbd5e2e60a0adbd1b2efab061640000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000084b61d27f60000000000000000000000000acf75e7c29bd27f1cd6ed47c8769c61bc6a68810000000000000000000000000000000000000000000000000006379da05b600000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004189f4d1fe2626f881082f93a336e51d6fad4faecfaffb843307c6c658bc4ae3ac737b6ece16b3c206b2d26efa8ca1ee6ac85e6a1e984456b7282404592e3d12e31c00000000000000000000000000000000000000000000000000000000000000"))
-        .maxPriorityFeePerGas(Wei.wrap(Bytes.fromHexString("5cb50683")))
-        .maxFeePerGas(Wei.wrap(Bytes.fromHexString("0x5cb5068b")))
-        .gasLimit(1396683L)
-        .chainId(BigInteger.valueOf(59144))
-        .build();
-
+    var tx =
+        new Transaction.Builder()
+            .signature(sig)
+            .type(TransactionType.EIP1559)
+            .sender(Address.fromHexString("0x43370108f30ee5ed54a9565f37af3be8502903f5"))
+            .to(Address.fromHexString("0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789"))
+            .nonce(14261)
+            .value(Wei.ZERO)
+            .payload(
+                Bytes.fromHexString(
+                    "0x1fad948c000000000000000000000000000000000000000000000000000000000000004000000000000000000000000043370108f30ee5ed54a9565f37af3be8502903f50000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000051ce6a8e868fa0c928346ac7489a74432c44bc933db076b8929744925f16e35a14de0c7e122e512efe282e76000000000000000000000000000000000000000000000000000000000000000000000000000001600000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000007a120000000000000000000000000000000000000000000000000000000000006b08a000000000000000000000000000000000000000000000000000000000000b1580000000000000000000000000000000000000000000000000000000065fa53ff000000000000000000000000000000000000000000000000000000005cb5068300000000000000000000000000000000000000000000000000000000000002c000000000000000000000000000000000000000000000000000000000000002e000000000000000000000000000000000000000000000000000000000000000783fbe35a874284e41c955331a363c1ea085301a8dd8fd8f440000000000000000000000001708dc05842c8fbd5e2e60a0adbd1b2efab061640000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000084b61d27f60000000000000000000000000acf75e7c29bd27f1cd6ed47c8769c61bc6a68810000000000000000000000000000000000000000000000000006379da05b600000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004189f4d1fe2626f881082f93a336e51d6fad4faecfaffb843307c6c658bc4ae3ac737b6ece16b3c206b2d26efa8ca1ee6ac85e6a1e984456b7282404592e3d12e31c00000000000000000000000000000000000000000000000000000000000000"))
+            .maxPriorityFeePerGas(Wei.wrap(Bytes.fromHexString("5cb50683")))
+            .maxFeePerGas(Wei.wrap(Bytes.fromHexString("0x5cb5068b")))
+            .gasLimit(1396683L)
+            .chainId(BigInteger.valueOf(59144))
+            .build();
 
     // Process block with this tx
     final BlockDataGenerator gen = new BlockDataGenerator();
-    final Block block = gen.block(BlockDataGenerator.BlockOptions
-        .create()
-        .addTransaction(tx)
-        .hasOmmers(false)
-        //.setStateRoot(Hash.fromHexString("0xc70a52d661bf92297e17d79b0cb04c508f3767acecf6145171884a82ea5e8a9d"))
-        .setStateRoot(Hash.fromHexString("0x543543c7c19059bbc9c384ab7344bc281fe08938732480a1bc48d01e3ba8090b"))
-    );
-    final BlockProcessor processor = setupUtil
-        .getProtocolSchedule()
-        .getByBlockHeader(
-            blockchain.getChainHeadHeader())
-        .getBlockProcessor();
+    final Block block =
+        gen.block(
+            BlockDataGenerator.BlockOptions.create()
+                .addTransaction(tx)
+                .hasOmmers(false)
+                // .setStateRoot(Hash.fromHexString("0xc70a52d661bf92297e17d79b0cb04c508f3767acecf6145171884a82ea5e8a9d"))
+                .setStateRoot(
+                    Hash.fromHexString(
+                        "0x543543c7c19059bbc9c384ab7344bc281fe08938732480a1bc48d01e3ba8090b")));
+    final BlockProcessor processor =
+        setupUtil
+            .getProtocolSchedule()
+            .getByBlockHeader(blockchain.getChainHeadHeader())
+            .getBlockProcessor();
 
-    final BlockProcessingResult result = processor.processBlock(
-        spyProtocolContext,
-        blockchain,
-        worldState,
-        block);
+    final BlockProcessingResult result =
+        processor.processBlock(spyProtocolContext, blockchain, worldState, block);
 
     // Assert transaction is present
     assertEquals(1, result.getReceipts().size());
@@ -350,21 +352,21 @@ public class ZkBlockImportTracerProviderTest {
     assertEquals(0, receipt.getStatus());
 
     var create2Address = Address.fromHexString("0x51ce6a8e868fa0c928346ac7489a74432c44bc93");
-    var create2StorageSeen = capturedTracer.get().getStoragesSeenByHubForRelativeBlock(1)
-        .get(create2Address);
+    var create2StorageSeen =
+        capturedTracer.get().getStoragesSeenByHubForRelativeBlock(1).get(create2Address);
     assertNotNull(create2StorageSeen);
 
     var trieLog = capturedTrieLog.get();
     var create2TrielogSeen = trieLog.getStorageChanges().get(create2Address);
     assertNotNull(create2TrielogSeen);
 
-    assertEquals(
-        create2StorageSeen.size(),
-        create2TrielogSeen.size());
+    assertEquals(create2StorageSeen.size(), create2TrielogSeen.size());
     // known reverted storage slot key:
-    final Bytes32 revertedSlotKey = Bytes32.fromHexString("0x322cf19c484104d3b1a9c2982ebae869ede3fa5f6c4703ca41b9a48c76ee0300");
+    final Bytes32 revertedSlotKey =
+        Bytes32.fromHexString("0x322cf19c484104d3b1a9c2982ebae869ede3fa5f6c4703ca41b9a48c76ee0300");
     assertTrue(create2StorageSeen.contains(revertedSlotKey));
-    var trieLogSlotVal = create2TrielogSeen.get(new StorageSlotKey(UInt256.fromBytes(revertedSlotKey)));
+    var trieLogSlotVal =
+        create2TrielogSeen.get(new StorageSlotKey(UInt256.fromBytes(revertedSlotKey)));
     assertNotNull(trieLogSlotVal);
   }
 }
